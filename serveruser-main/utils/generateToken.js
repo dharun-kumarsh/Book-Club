6ba41c0c115370478b05e7881502a71cfa09719c
SANJAY222-2006 <sanjay236002@gmail.com>
2025-03-31 13:54:57 +0530

    Delete serveruser-main directory
    deleted postgresql configured server from our project

--- serveruser-main/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: '1h', // You can adjust the expiration time
-  });
-};
-
-module.exports = generateToken;
