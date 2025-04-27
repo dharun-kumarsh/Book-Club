f1d2c36921faecd1a27bd705370f76a2c0187ac7
SANJAY222-2006 <sanjay236002@gmail.com>
2025-04-27 17:38:34 +0530

    dd

--- services/globalApi.js
+++ services/globalApi.js
@@ -1,4 +1,29 @@
 import axios from "axios";
+
 const api = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "http://localhost:3000",
 });
+
+// API Endpoints for Authentication
+
+// POST method for register
+export const registerData = async (userData) => {
+  try {
+    const response = await api.post("/api/auth/register", userData); // Pass userData as the request body
+    return response.data;
+  } catch (error) {
+    console.error("Error during registration:", error);
+    throw error;
+  }
+};
+
+// POST method for login
+export const loginData = async (loginCredentials) => {
+  try {
+    const response = await api.post("/api/auth/login", loginCredentials); // Pass login credentials as the request body
+    return response.data;
+  } catch (error) {
+    console.error("Error during login:", error);
+    throw error;
+  }
+};
